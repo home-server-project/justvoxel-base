a997072743a963fbd0b1909aa59605726d09c65c
iegorch86 <66792505+iegorch86@users.noreply.github.com>
2026-09-22 19:18:14 -0400

    Align VM validation with Base storage tools

--- build_files/validate/vm.sh
+++ build_files/validate/vm.sh
@@ -5,7 +5,7 @@ test "$(cat /usr/lib/justvoxel/variant)" = "justvoxel-vm"
 
 # VM keeps network-share clients and guarded partition tooling because a second
 # virtual disk or NFS/SMB backup target is part of the supported storage model.
-rpm -q nfs-utils cifs-utils parted xfsprogs util-linux >/dev/null
+rpm -q nfs-utils cifs-utils parted xfsprogs btrfs-progs util-linux >/dev/null
 
 # Validate the JustVoxel VM variant delta rather than rejecting every package
 # with hardware-related functionality. AlmaLinux minimal-plus already includes
@@ -15,7 +15,7 @@ rpm -q nfs-utils cifs-utils parted xfsprogs util-linux >/dev/null
 # The packages below are JustVoxel physical-hardware administration additions
 # that should stay out of the VM variant.
 for package in \
-    nut nut-client btrfs-progs smartmontools smartmontools-selinux nvme-cli \
+    nut nut-client smartmontools smartmontools-selinux nvme-cli \
     lm_sensors ethtool usbutils dmidecode fwupd-efi \
     NetworkManager-wifi amd-ucode-firmware atheros-firmware \
     brcmfmac-firmware iwlwifi-dvm-firmware iwlwifi-mvm-firmware realtek-firmware \
