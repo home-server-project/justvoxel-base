8b15f450935935c43b264f99c8f8b995bd81025b
iegorch86 <66792505+iegorch86@users.noreply.github.com>
2026-09-22 12:30:54 -0400

    Validate umount for storage browser actions [skip ci]

--- build_files/build-common.sh
+++ build_files/build-common.sh
@@ -104,7 +104,7 @@ install -m0755 /ctx/build_files/validate/vm.sh /usr/libexec/justvoxel/health/vm
 
 for cmd in \
     bootc podman skopeo nmcli nmtui nm-hsp resolvectl firewall-cmd sshd sudo just mjust \
-    tailscale netbird curl jq findmnt mountpoint flock mkfs.xfs mount.nfs mount.cifs \
+    tailscale netbird curl jq findmnt mountpoint flock mkfs.xfs mount.nfs mount.cifs umount \
     lsblk blkid wipefs parted partprobe udevadm qemu-ga vmtoolsd iperf3 micro spf python3 btop; do
     command -v "${cmd}"
 done
